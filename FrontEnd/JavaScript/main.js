2ef9e38aa22b4fd6aa52bd57fe2eb5bfe20e02af
marc135169 <marc.tetart2511@gmail.com>
2024-08-31 19:55:58 +0200

    Generate Base Gallery

--- FrontEnd/JavaScript/main.js
+++ FrontEnd/JavaScript/main.js
@@ -1,15 +1,48 @@
 // Get all Works from API
+let works;
+let categories;
+
 try {
     const responseWorks = await fetch('http://localhost:5678/api/works');    
-    const works = await responseWorks.json(); 
-    console.log(works); 
+    works = await responseWorks.json(); 
+    //console.log(works); 
     
     const responseCategories = await fetch('http://localhost:5678/api/categories');
-    const categories = await responseCategories.json();
-    console.log(categories);
+    categories = await responseCategories.json();
+    //console.log(categories);
 }
 catch (error) {
     throw new Error(error);
 }
+
+//Generate Gallery
+
+function generateGallery (workList){
+    
+    const galleryElement = document.querySelector('.gallery');
+    
+    for (let i = 0; i < works.length; i ++) {
+        const item = workList[i];
+        console.log(item);
+        
+        const figureElement = document.createElement('figure');
+        
+        const imgElement = document.createElement('img');    
+        imgElement.src = item.imageUrl;
+        imgElement.alt = item.title;
+        
+        const figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.innerText = item.title;
+        
+        galleryElement.appendChild(figureElement);
+        figureElement.appendChild(imgElement);
+        figureElement.appendChild(figcaptionElement);        
+    }    
+}
+
+generateGallery(works);
+
+
+
 
 
