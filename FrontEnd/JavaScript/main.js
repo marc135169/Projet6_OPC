e8ed2ba08dc56b83417822b63577f1840ef358e2
marc135169 <marc.tetart2511@gmail.com>
2024-09-01 22:36:56 +0200

    Filter button base

--- FrontEnd/JavaScript/main.js
+++ FrontEnd/JavaScript/main.js
@@ -1,22 +1,46 @@
 // Get all Works from API
+import response from "assert";
+
 let works;
 let categories;
 
 try {
     const responseWorks = await fetch('http://localhost:5678/api/works');    
-    works = await responseWorks.json(); 
-    //console.log(works); 
+    if (!responseWorks.ok) throw new Error(`Error while fetching works`);    
+    works = await responseWorks.json();     
     
     const responseCategories = await fetch('http://localhost:5678/api/categories');
+    if (!responseCategories.ok) throw new Error(`Error while fetching categories`);
     categories = await responseCategories.json();
-    //console.log(categories);
+    
 }
 catch (error) {
-    throw new Error(error);
+    console.error('Error: ', error.message);
 }
+
+
+
+//Create Filters
+const filtersSection = document.querySelector('.filters')
+
+for (let i = 0; i < 4; i++){
+    const btnElement = document.createElement('button');
+    btnElement.classList.add('filters-btn');
+    
+    filtersSection.appendChild(btnElement);
+}
+
+
+
 
 //Generate Gallery
 
+/**
+ * @author Tetart_Marc
+ * @param workList a list of elements to display
+ * @return void
+ * @description This Function take a list of elements and display it in the gallery div
+ */
 function generateGallery (workList){
     
     const galleryElement = document.querySelector('.gallery');
