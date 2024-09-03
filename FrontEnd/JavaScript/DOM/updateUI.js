e8504b8252011e5e9214033555df2d17f04dde7f
marc135169 <marc.tetart2511@gmail.com>
2024-09-03 11:14:35 +0200

    Reformat login fonction

---
+++ FrontEnd/JavaScript/DOM/updateUI.js
@@ -0,0 +1,100 @@
+const loginNavElement = document.querySelector('.site_login');
+const loginSectionElement = document.querySelector('.login');
+const filtersSectionElement = document.querySelector('.filters');
+const mainElement = document.querySelector('main');
+const bgModalElement = document.querySelector('.bg_modal');
+const modalElement = document.querySelector('.modal');
+const xElement = document.querySelector('.exitX');
+
+
+export function addButtonModifier(){    
+    
+    const portfolioH2Element = document.querySelector('#portfolio h2');
+    
+    const modifButton = document.createElement("button");
+    const divButton = document.createElement("div");
+    const iconElement = document.createElement("i");
+
+    divButton.classList.add("buttonDivModifier");
+    modifButton.innerText = 'modifier'
+    iconElement.classList.add('fa-regular', 'fa-pen-to-square');
+    
+
+    portfolioH2Element.insertAdjacentElement('afterend', divButton);
+    divButton.appendChild(iconElement);
+    divButton.appendChild(modifButton); 
+    
+    setListener(divButton);
+}
+
+loginNavElement.addEventListener('click', () => {    
+    if (loginNavElement.innerText === 'login') {
+        hideElement(mainElement);
+        
+        displayElement(loginSectionElement);   
+        
+    }else if(loginNavElement.innerText === 'logout') {
+        const divModifButtonElement = document.querySelector('.buttonDivModifier');
+        
+        hideElement(loginSectionElement);
+        hideElement(divModifButtonElement);
+             
+        displayElement(mainElement);
+        displayElement(filtersSectionElement);
+        
+        changeLoginLogout(); 
+    }
+});
+
+export function changeLoginLogout(){
+    if (loginNavElement.innerText === 'login') {
+        loginNavElement.innerText = 'logout';
+    } else {
+        loginNavElement.innerText = 'login';
+    }
+}
+
+export function displayElement(elementToDisplay){
+    
+    switch(elementToDisplay){
+        case bgModalElement:
+            elementToDisplay.style.display = 'flex';
+            break;
+        case loginSectionElement:
+            elementToDisplay.style.display = 'flex';
+            break;
+        case filtersSectionElement:
+            elementToDisplay.style.display = 'flex';
+            break;
+        case mainElement:
+            elementToDisplay.style.display = 'block';
+            break;
+        default:
+            console.log('element not found');
+    }
+}
+
+export function hideElement(elementToHide){    
+    elementToHide.style.display = 'none';  
+}
+
+
+function setListener(element){
+    if (element.className === 'buttonDivModifier') {}
+    element.addEventListener('click', () => {
+        displayElement(bgModalElement);        
+    })
+}
+
+//Modal
+bgModalElement.addEventListener('click', (event) => {
+    if (event.target === bgModalElement) {
+        hideElement(bgModalElement);        
+    }
+})
+
+xElement.addEventListener('click', () => {
+    
+    hideElement(bgModalElement);
+})
+
--- FrontEnd/JavaScript/login.js
+++ FrontEnd/JavaScript/login.js
@@ -1,25 +1,13 @@
-import {addButtonModifier} from "./DOM/modifButtonDOM.js";
+import {addButtonModifier} from "./DOM/updateUI.js";
+import {changeLoginLogout} from "./DOM/updateUI.js";
+import {displayElement} from "./DOM/updateUI.js";
+import {hideElement} from "./DOM/updateUI.js";
 
 const loginNavElement = document.querySelector('.site_login');
 const loginSectionElement = document.querySelector('.login');
 const filtersSectionElement = document.querySelector('.filters');
 const mainElement = document.querySelector('main');
 
-
-loginNavElement.addEventListener('click', () => {  
-    console.log(loginNavElement.innerText);
-    if (loginNavElement.innerText === 'login') {
-        loginNavElement.innerText = 'logout'
-        mainElement.style.display = 'none';
-        loginSectionElement.style.display = 'flex';
-    }else if(loginNavElement.innerText === 'logout') {
-        loginNavElement.innerText = 'login'
-        loginSectionElement.style.display = 'none';        
-        mainElement.style.display = 'block';
-        filtersSectionElement.style.display = 'flex';
-        changeLoginLogout();
-    }
-});
 
 document.querySelector('.login').addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -41,21 +29,20 @@ document.querySelector('.login').addEventListener('submit', async (e) => {
         
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            console.log(data); 
             
             sessionStorage.setItem('authToken', data.token);
-            mainElement.style.display = 'block';
-            loginSectionElement.style.display = 'none';
-            filtersSectionElement.style.display = 'none';
+            
+            displayElement(mainElement);
+            hideElement(loginSectionElement);
+            hideElement(filtersSectionElement);            
             addButtonModifier();
+            changeLoginLogout();
+            
+            
             
         } else {
             console.error('connexion error');           
         }    
     });
 
-
-function changeLoginLogout(){
-    const divButtonElement = document.querySelector('.buttonDivModifier');
-    divButtonElement.innerHTML = '';
-}
